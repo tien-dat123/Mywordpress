b9e6537124d1d54220b7521dde5d7119c46cdf48
tien-dat123 <datlu2020@gmail.com>
2022-11-02 21:06:11 +0700

    upload

---
+++ wp-admin/post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://tien-dat123.github.io/Mywordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A7000%2Fmyproject%2Fwordpress%2Fwp-admin%2Fpost.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://tien-dat123.github.io/Mywordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A7000%2Fmyproject%2Fwordpress%2Fwp-admin%2Fpost.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://tien-dat123.github.io/Mywordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A7000%2Fmyproject%2Fwordpress%2Fwp-admin%2Fpost.php&reauth=1">https://tien-dat123.github.io/Mywordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A7000%2Fmyproject%2Fwordpress%2Fwp-admin%2Fpost.php&reauth=1</a></p>
+	</body>
+</html>
