b72e0522dfd4ac1bb47f168f2eeb02daff4e2126
tien-dat123 <datlu2020@gmail.com>
2022-11-03 11:36:05 +0700

    upload

--- wp-login.php
+++ wp-login.php
@@ -1,77 +1,75 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html lang="vi">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Log In &lsaquo; My Project &#8212; WordPress</title>
+	<title>Đăng nhập &lsaquo; My Project &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
-<link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="https://tien-dat123.github.io/Mywordpress/wp-includes/css/dashicons.min.css?ver=6.0.3" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://tien-dat123.github.io/Mywordpress/wp-includes/css/buttons.min.css?ver=6.0.3" media="all">
-<link rel="stylesheet" id="forms-css" href="https://tien-dat123.github.io/Mywordpress/wp-admin/css/forms.min.css?ver=6.0.3" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://tien-dat123.github.io/Mywordpress/wp-admin/css/l10n.min.css?ver=6.0.3" media="all">
-<link rel="stylesheet" id="login-css" href="https://tien-dat123.github.io/Mywordpress/wp-admin/css/login.min.css?ver=6.0.3" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://tien-dat123.github.io/Mywordpress/wp-includes/css/dashicons.min.css?ver=6.1" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://tien-dat123.github.io/Mywordpress/wp-includes/css/buttons.min.css?ver=6.1" media="all">
+<link rel="stylesheet" id="forms-css" href="https://tien-dat123.github.io/Mywordpress/wp-admin/css/forms.min.css?ver=6.1" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://tien-dat123.github.io/Mywordpress/wp-admin/css/l10n.min.css?ver=6.1" media="all">
+<link rel="stylesheet" id="login-css" href="https://tien-dat123.github.io/Mywordpress/wp-admin/css/login.min.css?ver=6.1" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
-	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<body class="login no-js login-action-login wp-core-ui  locale-vi">
 	<script type="text/javascript">
 		document.body.className = document.body.className.replace('no-js','js');
 	</script>
 		<div id="login">
-		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+		<h1><a href="https://vi.wordpress.org/">Xây dựng bằng WordPress</a></h1>
 	
 		<form name="loginform" id="loginform" action="http://localhost:7000/myproject/wordpress/wp-login.php" method="post">
 			<p>
-				<label for="user_login">Username or Email Address</label>
+				<label for="user_login">Tên người dùng hoặc Địa chỉ Email</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username">
 			</p>
 
 			<div class="user-pass-wrap">
-				<label for="user_pass">Password</label>
+				<label for="user_pass">Mật khẩu</label>
 				<div class="wp-pwd">
 					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password">
-					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Hiện mật khẩu">
 						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
 					</button>
 				</div>
 			</div>
-						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Tự động đăng nhập</label></p>
 			<p class="submit">
-				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Đăng nhập">
 									<input type="hidden" name="redirect_to" value="http://localhost:7000/myproject/wordpress/wp-admin/post.php">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="https://tien-dat123.github.io/Mywordpress/my-account/lost-password/">Lost your password?</a>
-			</p>
+				<a href="https://tien-dat123.github.io/Mywordpress/my-account/lost-password/">Bạn quên mật khẩu?</a>			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="https://tien-dat123.github.io/Mywordpress/">&larr; Go to My Project</a>		</p>
+			<a href="https://tien-dat123.github.io/Mywordpress/">&larr; Quay lại My Project</a>		</p>
 			</div>
 				<div class="language-switcher">
 				<form id="language-switcher" action="" method="get">
 
 					<label for="language-switcher-locales">
 						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
-						<span class="screen-reader-text">Language</span>
+						<span class="screen-reader-text">Ngôn ngữ</span>
 					</label>
 
 					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
-<option value="vi" lang="vi" data-installed="1">Tiếng Việt</option></select>
+<option value="vi" lang="vi" selected data-installed="1">Tiếng Việt</option></select>
 					
 											<input type="hidden" name="redirect_to" value="http://localhost:7000/myproject/wordpress/wp-admin/post.php">
 					
 					
-						<input type="submit" class="button" value="Change">
+						<input type="submit" class="button" value="Thay đổi">
 
 					</form>
 				</div>
-				<script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+				<script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/jquery/jquery.min.js?ver=3.6.1" id="jquery-core-js"></script>
 <script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script id="zxcvbn-async-js-extra">
 var _zxcvbnSettings = {"src":"http:\/\/localhost:7000\/myproject\/wordpress\/wp-includes\/js\/zxcvbn.min.js"};
@@ -79,24 +77,38 @@ var _zxcvbnSettings = {"src":"http:\/\/localhost:7000\/myproject\/wordpress\/wp-
 <script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
 <script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
 <script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/dist/hooks.min.js?ver=c6d64f2cb8f5c6bb49caca37f8828ce3" id="wp-hooks-js"></script>
-<script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/dist/i18n.min.js?ver=ebee46757c6a411e38fd079a7ac71d94" id="wp-i18n-js"></script>
+<script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
+<script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
 <script id="wp-i18n-js-after">
-wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
 <script id="password-strength-meter-js-extra">
-var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+var pwsL10n = {"unknown":"Mật khẩu mạnh không xác định","short":"Rất yếu","bad":"Yếu","good":"Trung bình","strong":"Mạnh","mismatch":"Mật khẩu không khớp"};
 </script>
-<script src="https://tien-dat123.github.io/Mywordpress/wp-admin/js/password-strength-meter.min.js?ver=6.0.3" id="password-strength-meter-js"></script>
-<script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
+<script id="password-strength-meter-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2022-08-03 11:30:24+0000","generator":"GlotPress\/4.0.0-alpha.3","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"vi_VN"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s đã ngừng hoạt động từ phiên bản %2$s! Sử dụng thay thế bằng %3$s."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
+</script>
+<script src="https://tien-dat123.github.io/Mywordpress/wp-admin/js/password-strength-meter.min.js?ver=6.1" id="password-strength-meter-js"></script>
+<script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
 <script id="wp-util-js-extra">
 var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-ajax.php"}};
 </script>
-<script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/wp-util.min.js?ver=6.0.3" id="wp-util-js"></script>
+<script src="https://tien-dat123.github.io/Mywordpress/wp-includes/js/wp-util.min.js?ver=6.1" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"d588e25450"};
+var userProfileL10n = {"user_id":"0","nonce":"f4b37627bf"};
 </script>
-<script src="https://tien-dat123.github.io/Mywordpress/wp-admin/js/user-profile.min.js?ver=6.0.3" id="user-profile-js"></script>
+<script id="user-profile-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2022-08-03 11:30:24+0000","generator":"GlotPress\/4.0.0-alpha.3","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"vi_VN"},"Your new password has not been saved.":["Mật khẩu mới của bạn chưa được lưu."],"Show":["Hiển thị"],"Hide":["Ẩn đi"],"Show password":["Hiện mật khẩu"],"Hide password":["Ẩn mật khẩu"],"Confirm use of weak password":["Chấp nhận sử dụng mật khẩu yếu."]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
+</script>
+<script src="https://tien-dat123.github.io/Mywordpress/wp-admin/js/user-profile.min.js?ver=6.1" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
